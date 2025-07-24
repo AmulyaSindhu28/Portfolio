8735e358deafa314ce41239b22715b548dc939df
AmulyaSindhu28 <bejjankiamulyasindhu@gmail.com>
2025-07-24 16:12:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon=document.querySelector("#menu-icon");
+const navbar=document.querySelector(".navbar");
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle("fa-bars");
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+}
